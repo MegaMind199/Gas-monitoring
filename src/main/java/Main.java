88d38f00ba0430fe1489d855fe3c0047eda6c53b
THOR <elguja.abuladze@azry.com>
2024-11-14 15:42:00 +0400

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
 
     public static void main(String[] args) {
+
         AccountDAO accountDAO = new AccountDAO();
         String uniqueNumber = "UN12345";  // Example unique number
 
